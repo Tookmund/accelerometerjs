915f26c2e4c0bfbcaaeed22a22329dcbc3b167e9
Jacob Adams <jacob@tookmund.com>
2019-12-17 21:38:37 -0500

    ondevicemotion: fallback to old apis if we can't get the new ones working

--- ondevicemotion.js
+++ ondevicemotion.js
@@ -16,21 +16,43 @@ function getMotion(event) {
 
 // Device Motion request must come from a user-generated event
 function requestMotion() {
-	DeviceMotionEvent.requestPermission().then(response => {
-	  if (response == 'granted') {
-		  window.addEventListener("devicemotion", getMotion);
-		  let req =  document.getElementById("request");
-		  req.innerHTML = "End";
-		  req.onclick = endMotion;
-	  } else {
-		  document.getElementById("acceldata").innerHTML = "Need Device Motion!";
-	  }
-	});
+	try {
+		DeviceMotionEvent.requestPermission().then(response => {
+		  if (response == 'granted') {
+			  window.addEventListener("devicemotion", getMotion);
+			  let req =  document.getElementById("request");
+			  req.innerHTML = "End";
+			  req.onclick = endMotion;
+		  } else {
+			  document.getElementById("acceldata").innerHTML = "Need Device Motion!";
+		  }
+		});
+	}
+	// Fallback to just trying to get device motion events without permission
+	catch(error) {
+		try {
+			window.addEventListener("devicemotion", getMotion);
+		}
+		// Fallback to old API
+		catch (err) {
+			try {
+				window.ondevicemotion = getMotion;
+			}
+			// Give up
+			catch (e) {
+				document.getElementById("acceldata").innerHTML = "No Access to device motion!";
+			}
+		}
+	}
 }
 document.getElementById("request").onclick = requestMotion;
 
 function endMotion() {
-	window.removeEventListener("devicemotion", getMotion);
+	try {
+		window.removeEventListener("devicemotion", getMotion);
+	} catch(e) {
+		window.ondevicemotion = null;
+	}
 	let req = document.getElementById("request");
 	req.onclick = requestMotion;
 	req.innerHTML = "Begin";
