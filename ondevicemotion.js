2a98b38bd487a4eea34e28b8fc6a61d7d42444e1
Jacob Adams <jacob@tookmund.com>
2019-12-17 21:05:50 -0500

    ondevicemotion: Remove console logging
    we can't see it on mobile anyway and can't test this on desktop

--- ondevicemotion.js
+++ ondevicemotion.js
@@ -8,11 +8,10 @@ function getMotion(event) {
 	if (Math.abs(x) > Math.abs(maxX)) maxX = x;
 	if (Math.abs(y) > Math.abs(maxY)) maxY = y;
 	if (Math.abs(z) > Math.abs(maxZ)) maxZ = z;
-	var str = "<p>X: "+x+"</p><p>Y: "+y+"</p><p>Z: "+z+"</p><p>"+
+	document.getElementById('acceldata').innerHTML =
+		"<p>X: "+x+"</p><p>Y: "+y+"</p><p>Z: "+z+"</p><p>"+
 		"Interval: "+event.interval+
 		"<p>Max X: "+maxX+"</p><p>Max Y: "+maxY+"</p><p>Max Z: "+maxZ+"</p>";
-	console.log(str);
-	document.getElementById('acceldata').innerHTML = str;
 }
 
 // Device Motion request must come from a user-generated event
