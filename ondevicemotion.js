218668d111c0595cc98c7b3841e90178f8e13e55
Jacob Adams <jacob@tookmund.com>
2019-12-17 20:37:45 -0500

    Add time interval to accelerometer data

--- ondevicemotion.js
+++ ondevicemotion.js
@@ -2,7 +2,7 @@ function getMotion(event) {
 	var x = event.accelerationIncludingGravity.x;
 	var y = event.accelerationIncludingGravity.y;
 	var z = event.accelerationIncludingGravity.z;
-	var str = "<p>X: "+x+"</p><p>Y: "+y+"</p><p>Z: "+z+"</p>";
+	var str = "<p>X: "+x+"</p><p>Y: "+y+"</p><p>Z: "+z+"</p><p>Interval: "+event.interval;
 	console.log(str);
 	document.getElementById('acceldata').innerHTML = str;
 }
